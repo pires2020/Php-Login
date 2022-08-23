ef6f322cd348bf4666fb84627098777ed72d8545
Lucas Antunes Pires <64935655+pires2020@users.noreply.github.com>
2022-08-23 17:03:55 -0300

    novo projeto

---
+++ home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    body{
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
+<body>
+    <h1>SEILA1</h1>
+    <h2>eu pires</h2>
+    <div class="box">
+        <a href="">login</a>
+        <a href="">cadastre-se</a>
+    </div>
+</body>
+</html>
